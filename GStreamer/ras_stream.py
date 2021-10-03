5f01a91b960da994b58e37e9dc38347978ce7a81
name <qkdkran123@gmail.com>
2021-10-03 14:42:36 +0900

    edit

---
+++ GStreamer/ras_stream.py
@@ -0,0 +1,4 @@
+import os as os
+
+os.system("ls -al")
+os.system("raspivid -n -t 0 -vf -h 720 -w 1280 -fps 25 -b 2000000 -o - |gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=192.168.1.41 port=5000")
--- GStreamer/stream.py
+++
@@ -1,4 +0,0 @@
-import os as os
-
-os.system("ls -al")
-os.system("ls -al | grep System* > ras_gstream")
