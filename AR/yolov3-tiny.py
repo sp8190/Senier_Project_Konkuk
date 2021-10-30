6c5dedd756cdcb7a502f71f550bb0ea0b2fe5ccf
SilverLight96 <tim960321@naver.com>
2021-10-30 15:46:32 +0900

    Update yolov3-tiny.py

--- AR/yolov3-tiny.py
+++ AR/yolov3-tiny.py
@@ -7,6 +7,7 @@
 import cv2
 import numpy as np
 
+#device 내장캠, whT=영상크기
 device = 0
 cap = cv2.VideoCapture(device)
 whT = 320
@@ -20,6 +21,7 @@ classesFile = 'coco.names'
 with open(classesFile, 'rt') as f:
         classNames = f.read().split('\n')
 
+#모델 불러오기
 model_config = 'yolov3-tiny.cfg'
 model_weights = 'yolov3-tiny.weights'
 
@@ -27,6 +29,7 @@ net = cv2.dnn.readNetFromDarknet(model_config, model_weights)
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
+#객체 식별 함수
 def findObjects(outputs, img):
         hT, wT, cT = img.shape
         bbox = []
@@ -45,9 +48,11 @@ def findObjects(outputs, img):
                     classIds.append(classId)
                     confs.append(float(confidence))
           
+        #중복된 박스생성을 막기위한 NMS알고리즘
         indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold_2, nmsThreshold)
         #print(f"식별된 대상 : {len(indices)} 개")
     
+    #걸러진 데이터를 토대로 이미지에 박스 및 텍스트 생성
         for i in indices:
             i = i[0]
             box = bbox[i]
@@ -55,13 +60,13 @@ def findObjects(outputs, img):
             cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
             cv2.putText(img, f"{classNames[classIds[i]].upper()} {int(confs[i]*100)}%",
                      (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-            
+ 
 while True:
     success, img = cap.read()
     
     if not success:
         break
-    
+ #성공했을때 true값 반환   
     blob = cv2.dnn.blobFromImage(img, 1/255, (whT, whT), [0,0,0], True, crop=False)
     net.setInput(blob)
     
@@ -72,6 +77,9 @@ while True:
     findObjects(outputs, img)
     
     cv2.imshow("Image", img)
+    
+    
+    #q 누르면 종료
     if cv2.waitKey(1) == ord('q'): 
         break
 cap.release()
