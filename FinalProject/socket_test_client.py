a85169047f1fc0cbd2763c84e17879b6382492c7
sp8190 <56158143+sp8190@users.noreply.github.com>
2021-11-04 09:34:00 +0900

    face.py 랑 socket_test.py 합침

--- FinalProject/socket_test_client.py
+++ FinalProject/socket_test_client.py
@@ -6,6 +6,7 @@ import subprocess
 import os
 from datetime import datetime
 import cv2 as cv2
+import numpy as np
 import math
 from queue import Queue
 from time import sleep
@@ -30,14 +31,171 @@ def opencv_img():
     
     url = 'rtsp://192.168.1.165:8554/test'
     cap = cv2.VideoCapture(url)
+    YOLO_net = cv2.dnn.readNet("yolov3-tiny.weights","yolov3-tiny.cfg")
+
+    #roi = img[100:600, 200:400]
+    #width, height, channel = img.shape
+
+    #각 사물에 해당하는 이미지(AR) 불러오기
+    #1번째 이미지: person
+    logo = cv2.imread('user.jpg') #person
+    #2번째 이미지: refrigerator
+    refri = cv2.imread('refri.jpg') #냉장고
+    #3번째 이미지: bottle
+    bottle = cv2.imread('bottle.jpg') #병
+    #이미지 크기 조절
+    size = 20
+    logo = cv2.resize(logo, (size, size))
+    refri = cv2.resize(refri,(size,size))
+    bottle = cv2.resize(bottle,(size,size))
+
+    img2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
+
+    img2gray_2 = cv2.cvtColor(refri, cv2.COLOR_BGR2GRAY)
+    ret_2, mask_2 = cv2.threshold(img2gray_2, 1, 255, cv2.THRESH_BINARY)
+
+    img2gray_3 = cv2.cvtColor(bottle, cv2.COLOR_BGR2GRAY)
+    ret_3, mask_bottle = cv2.threshold(img2gray_3, 1, 255, cv2.THRESH_BINARY)
+
+    # YOLO NETWORK 재구성
+    classes = []
+    with open("coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    layer_names = YOLO_net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in YOLO_net.getUnconnectedOutLayers()]
+
+    
     cv2.namedWindow('image')  #마우스 이벤트 영역 윈도우 생성
     cv2.setMouseCallback('image', mouse_callback)
 
     while(True):
         ret, frame = cap.read()    # Read 결과와 frame
-        
-        if(ret) :
-            cv2.imshow('image', frame)    # 컬러 화면 출력
+        h, w, c = frame.shape
+
+        # YOLO 입력
+        blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0),
+        True, crop=False)
+        YOLO_net.setInput(blob)
+        outs = YOLO_net.forward(output_layers)
+
+        class_ids = []
+        confidences = []
+        boxes = []
+
+        for out in outs:
+
+            for detection in out:
+
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+
+                if confidence > 0.5:
+                    # Object detected
+                    center_x = int(detection[0] * w)
+                    center_y = int(detection[1] * h)
+                    dw = int(detection[2] * w)
+                    dh = int(detection[3] * h)
+                    # Rectangle coordinate
+                    x = int(center_x - dw / 2)
+                    y = int(center_y - dh / 2)
+                    boxes.append([x, y, dw, dh])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.45, 0.4)
+
+
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                score = confidences[i]
+
+                # 경계상자와 클래스 정보 이미지에 입력
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
+                cv2.putText(frame, label, (x, y - 20), cv2.FONT_ITALIC, 0.5, 
+                (0, 0, 0), 1)
+                
+                #라벨 입력(정보 입력)
+                if label == 'person':
+                    cv2.putText(frame, "Konkuk Stu.", (x+w-100, y+15), cv2.FONT_ITALIC, 0.5, 
+                        (0, 0, 0), 1)
+                    
+                    #특정 위치에 이미지 불러오기
+                    if ret:
+                    # Flip the frame
+                        #frame = cv2.flip(frame, 1)
+
+                    # Region of Image (ROI), where we want to insert logo
+                    # 화면 밖으로 나가는 것을 방지하기 위함(x창: 640 y창: 480)
+                        if x>620:
+                            x=620
+                        elif x<0:
+                            x=0
+                        
+                        if y>460:
+                            y=460
+                        elif y<0:
+                            y=0
+                        roi = frame[y:y+20,x:x+20]
+                        
+                    
+                    # Set an index of where the mask is
+                        roi[np.where(mask)] = 0
+                        roi += logo
+                        
+                        
+                        #frame=cv2.flip(frame,1)
+                elif label == 'refrigerator':
+                    cv2.putText(frame, "store food", (x+w-100, y+15), cv2.FONT_ITALIC, 0.5, 
+                                (0, 0, 0), 1)
+                    
+                    if ret_2:
+                    
+                        #roi_refri = frame[y:y+20,x+w+100:x+w+120]
+                        if x>620:
+                            x=620
+                        elif x<0:
+                            x=0
+                        
+                        if y>460:
+                            y=460
+                        elif y<0:
+                            y=0
+                        roi_refri = frame[y:y+20,x-20:x]
+
+                        roi_refri[np.where(mask_2)] = 0
+                        roi_refri += refri
+    
+                elif label == 'bottle':
+                    cv2.putText(frame, "store drink", (x+w-100, y+15), cv2.FONT_ITALIC, 0.5, 
+                                (0, 0, 0), 1)
+                    
+                    if ret_3:
+                        if x>620:
+                            x=620
+                        elif x<0:
+                            x=0
+                        
+                        if y>460:
+                            y=460
+                        elif y<0:
+                            y=0
+                        #roi_refri = frame[y:y+20,x+w+100:x+w+120]
+                        roi_bottle = frame[y:y+20,x:x+20]
+                        
+                    # Set an index of where the mask is
+                        roi_bottle[np.where(mask_bottle)] = 0
+                        roi_bottle += refri
+
+                else:
+                    cv2.circle(frame, (x+w-10,y+10), 5, (0,0,255), -1)
+
+
+
+        cv2.imshow('image', frame)
 
         k = cv2.waitKey(1) & 0xFF
         if k == 27:    # ESC 키 눌러졌을 경우 종료
@@ -49,14 +207,14 @@ def opencv_img():
     cap.release()
     cv2.destroyAllWindows()
 
+    
 def calculate_distance():
     while(True):
         case = dis_queue.get()
         if case == "break":
             break
         degree = 0
-        if case == "server": # 라즈베리파이로부터 서보모터의 각도 값을 받아올 경우 사용
-            degree = disqueue.get()
+        # if case == "server": # 라즈베리파이로부터 서보모터의 각도 값을 받아올 경우 사용
 
         if case == "opencv": # 1550 기준
             x = dis_queue.get()
